e986ad1b9fa896f20d7cdd296d130d804f55ecfa
MarcusJones <jones.0bj3@gmail.com>
2019-08-25 14:15:41 +0200

    update final

--- frontend/src/app/app.service.ts
+++ frontend/src/app/app.service.ts
@@ -12,8 +12,8 @@ const httpOptions = {
 @Injectable()
 export class AppService {
   constructor(private httpClient: HttpClient ) { }
-  // baseUrl: string = 'http://localhost:80/';
-  baseUrl: string = 'http://10.0.4.254:8080/';
+  baseUrl: string = 'http://localhost:8080/';
+  // baseUrl: string = 'http://10.0.4.254:8080/';
 
   save(blob: URL): Observable<{}>{
     return this.httpClient.post(this.baseUrl, blob, httpOptions).pipe(
